9232ee1351b9ffb71ac5ebf087556a8dde9438bb
Laura Savinon <laurasaviinon@gmail.com>
2023-07-28 15:00:02 -0400

    More changes added on!

--- js/auth.js
+++ js/auth.js
@@ -60,60 +60,7 @@ async function logout() {
     throw error;
   } finally {
     window.localStorage.removeItem("login-data");
-    window.location.assign("/");
+    window.location.assign("../");
   }
 }
 
-// This function is already being used in the starter code for the
-// landing page, in order to process a user's login. READ this code,
-// and feel free to re-use parts of it for other `fetch()` requests
-// you may need to write.
-async function handleLoginForm(event) {
-  event.preventDefault();
-
-  const usernameInput = document.getElementById("username");
-  const passwordInput = document.getElementById("password");
-
-  const username = usernameInput.value.trim();
-  const password = passwordInput.value;
-
-  if (username === "" || password === "") {
-    alert("Please enter your username and password.");
-    return;
-  }
-
-  const loginButton = document.getElementById("loginButton");
-  loginButton.disabled = true;
-  loginButton.textContent = "Logging in...";
-
-  try {
-    const response = await fetch(`${apiBaseURL}/auth/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    });
-
-    if (!response.ok) {
-      throw new Error("Invalid username or password");
-    }
-
-    const { token } = await response.json();
-
-    const loginData = { username, token };
-    window.localStorage.setItem("login-data", JSON.stringify(loginData));
-
-    window.location.href = "../html/home.html";
-  } catch (error) {
-    console.log(error);
-    alert(error.message);
-  } finally {
-    loginButton.disabled = false;
-    loginButton.textContent = "Login";
-  }
-}
-
-// Add event listener to the login form
-const loginForm = document.getElementById("login");
-loginForm.addEventListener("submit", handleLoginForm);
--- js/registration.js
+++ js/registration.js
@@ -1,14 +1,15 @@
 "use strict";
 
 // Function to handle user registration
-async function register(username, password) {
+async function register(username, fullName, password) {
+  const loginData = getLoginData()
   try {
-    const response = await fetch(`${apiBaseURL}/auth/register`, {
+    const response = await fetch(`${apiBaseURL}/api/users`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({ username, fullName, password }),
     });
     const data = await handleAPIError(response);
     return data;
@@ -23,10 +24,12 @@ async function handleRegistrationForm(event) {
   event.preventDefault();
 
   const usernameInput = document.getElementById("username");
+  const fullnameInput = document.getElementById("fullname")
   const passwordInput = document.getElementById("password");
   const confirmPasswordInput = document.getElementById("confirmPassword");
 
   const username = usernameInput.value.trim();
+  const fullName = fullnameInput.value.trim();
   const password = passwordInput.value;
   const confirmPassword = confirmPasswordInput.value;
 
@@ -40,14 +43,14 @@ async function handleRegistrationForm(event) {
     return;
   }
 
-  const registerButton = document.getElementById("registerButton");
+  const registerButton = document.querySelector("#registration button");
   registerButton.disabled = true;
   registerButton.textContent = "Registering...";
 
   try {
-    await register(username, password);
+    await register(username, fullName, password);
     alert("Registration successful! You can now log in.");
-    window.location.href = "../html/landing.html";
+    window.location.href = "../html/login.html";
   } catch (error) {
     console.log(error);
     alert(error.message);
